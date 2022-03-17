1fc9fb649c353ad4d5257770e45a7a123655ae44
linzhu <lin.lyn.zhu@gmail.com>
2022-03-16 21:39:36 -0700

    connect -> get data -> write to local file

---
+++ fetcher.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const net = require('net');
+const conn = net.createConnection({
+  host: 'example.edu',
+  port: 80
+});
+conn.setEncoding('UTF8');
+conn.on('connect', () => {
+  console.log(`Connected to server!`);
+
+  conn.write(`GET / HTTP/1.1\r\n`);
+  conn.write(`Host: example.edu\r\n`);
+  conn.write(`\r\n`);
+});
+conn.on('data', (data) => {
+  console.log(`Downloaded and saved ${data.length} bytes to ./index.html`);
+  fs.writeFile('index.html', data, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    //file written successfully
+  });
+  conn.end();
+});
+
+
